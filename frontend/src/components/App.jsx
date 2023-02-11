d5dd877c0512b568bdcd4a0dd30451021baef918
Vidyasager GR <vidyasager162@gmail.com>
2023-02-11 10:19:42 +0530

    Change Folder Structure (again)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import Hero from "./Hero";
 
 function App() {
     return(
-        <>
-            <Header />
-            <Hero />
-            <Footer />
-        </>
+        <Home />
     );
 }
 
