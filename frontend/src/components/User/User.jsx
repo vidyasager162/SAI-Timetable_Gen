34d8ce5fd87528ce50dc28f5d342a1489faa5602
Vidyasager GR <vidyasager162@gmail.com>
2023-03-09 13:26:03 +0530

    Start User Dynamism

--- frontend/src/components/User/User.jsx
+++ frontend/src/components/User/User.jsx
@@ -1,77 +1,77 @@
 import React from "react";
 import UserList from "./UserList";
 
-function User() {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Vidyasager",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u2",
-      name: "Rituraj Pradhan",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u3",
-      name: "Vignesh Sai Sankalp Sham",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u4",
-      name: "Suman Chettri",
-      class: "III BBA",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u5",
-      name: "Agaventi Bhargav",
-      class: "III BBA",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u6",
-      name: "Harshit Goyal",
-      class: "II BBA",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u7",
-      name: "Saish Hebbar",
-      class: "III BBA",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u8",
-      name: "Siva Rama Varma",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u2",
-      name: "Rituraj Pradhan",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u2",
-      name: "Rituraj Pradhan",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-    {
-      id: "u2",
-      name: "Rituraj Pradhan",
-      class: "III BSc",
-      img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
-    },
-  ];
+function User(props) {
+  // const USERS = [
+  //   {
+  //     id: "u1",
+  //     name: "Vidyasager",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u2",
+  //     name: "Rituraj Pradhan",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u3",
+  //     name: "Vignesh Sai Sankalp Sham",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u4",
+  //     name: "Suman Chettri",
+  //     class: "III BBA",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u5",
+  //     name: "Agaventi Bhargav",
+  //     class: "III BBA",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u6",
+  //     name: "Harshit Goyal",
+  //     class: "II BBA",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u7",
+  //     name: "Saish Hebbar",
+  //     class: "III BBA",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u8",
+  //     name: "Siva Rama Varma",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u2",
+  //     name: "Rituraj Pradhan",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u2",
+  //     name: "Rituraj Pradhan",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  //   {
+  //     id: "u2",
+  //     name: "Rituraj Pradhan",
+  //     class: "III BSc",
+  //     img: "https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__340.jpg",
+  //   },
+  // ];
 
-  return <UserList items={USERS} />;
+  return <UserList users={props.users} />;
 }
 
 export default User;
--- frontend/src/components/User/UserCard.jsx
+++ frontend/src/components/User/UserCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import profileImage from "../../images/profile.jpg";
 
 function UserCard(props) {
   return (
     <div className="card h-100">
-      <img src={props.img} className="card-img-top" alt={props.name} />
+      <img src={profileImage} className="card-img-top" alt={props.name} />
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text">{props.class}</p>
